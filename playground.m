d792c906a8148092fd6cf385f52e68a15dd6c9ff
OrrAvrech <orravrech@gmail.com>
2018-09-23 18:10:58 +0300

    pg: fit affine transform

--- playground.m
+++ playground.m
@@ -18,5 +18,34 @@ diff = avg_captured - captured(:,:,1);
 imshow(diff, []);
 hold on
 plot(pts_diff(:,1),pts_diff(:,2), 'ro');
+hold off
 
 %% Fit Affine Transformation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% First, create an affine transform and a transformed (moving) image.
+% Then, use corner detection to recover the transform from the
+% extracted corner points of the fixed and moving images.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Affine Transformation - Translation, Scale, Rotation
+TS = [1 0 0; 0 0.5 0; 0 0 1];
+theta = 30; % degrees
+R  = [cosd(theta) sind(theta) 0; -sind(theta) cosd(theta) 0; 0 0 1];
+tform  = affine2d(TS * R);
+timage = imwarp(ch_0, tform);
+figure;
+imshowpair(ch_0,timage);
+figure;
+imshow(timage, []);
+% Detect Corners -- Transformed Image
+[pts_t, bs_t] = detectCheckerboardPoints(timage);
+hold on
+plot(pts_t(:,1), pts_t(:,2), 'ro');
+hold off
+% Fit Geometric Transform
+tform_fit = fitgeotrans(pts_t,imagePoints,'affine');
+image_recon = imwarp(timage, tform_fit, 'OutputView', imref2d(size(ch_0)));
+% Reconstructed Image
+figure;
+imshowpair(ch_0,image_recon,'montage');
+
