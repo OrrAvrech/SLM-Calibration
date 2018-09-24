8c68bc77625a45a970b9a2ce257ff5b8f73b79ba
OrrAvrech <orravrech@gmail.com>
2018-09-24 15:50:44 +0300

    pg: three boundary corner pts fit

--- playground.m
+++ playground.m
@@ -10,16 +10,6 @@ imshow(ch_0, []);
 hold on
 plot(imagePoints(:,1),imagePoints(:,2), 'ro');
 
-%% Diff Image Corner Detection
-captured = avi2gray('check_10_0.avi');
-avg_captured = mean(captured, 3);
-diff = avg_captured - captured(:,:,1);
-[pts_diff, ~] = detectCheckerboardPoints(diff);
-imshow(diff, []);
-hold on
-plot(pts_diff(:,1),pts_diff(:,2), 'ro');
-hold off
-
 %% Fit Affine Transformation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % First, create an affine transform and a transformed (moving) image.
@@ -49,3 +39,21 @@ image_recon = imwarp(timage, tform_fit, 'OutputView', imref2d(size(ch_0)));
 figure;
 imshowpair(ch_0,image_recon,'montage');
 
+%% Diff Image Corner Detection
+captured = avi2gray('check_10_0.avi');
+avg_captured = mean(captured, 3);
+diff = avg_captured - captured(:,:,1);
+[pts_diff, ~] = detectCheckerboardPoints(diff);
+imshow(diff, []);
+hold on
+plot(pts_diff(:,1),pts_diff(:,2), 'ro');
+hold off
+
+%% 3 Boundary Corner Pts Fit
+inputPts  = [150 1540; 150 400; 900 400];
+movingPts = [1241 54; 54 71; 64 985]; 
+tform_boundaries = fitgeotrans(movingPts, inputPts, 'affine');
+% Interactive Crop
+diffPatch = imcrop(diff);
+diffPatch_recon = imwarp(diffPatch, tform_boundaries);
+imshowpair(diffPatch, diffPatch_recon, 'montage');
